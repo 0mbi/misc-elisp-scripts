b25418f552a50953194c3cffaad8d8582b583132
Jeschli <jeschli@gmail.com>
2019-12-20 22:36:11 +0100

    Add some crap

--- components.el
+++ components.el
@@ -1,6 +1,6 @@
 (defun mh/components()
     (let ((lines '()))
-    (while (search-forward "#[derive(Component)]" nil t)
+    (while (re-search-forward "#\\[derive(.*Component.*)]" nil t)
       (skip-chars-forward "\n")
       (let (
            (actual-line (thing-at-point 'line))
@@ -24,3 +24,42 @@
    (save-excursion
      (mapcar 'message (mh/components))
      ))
+
+
+(ert-deftest should-match-string ()
+  (should (not (equal (string-match "#\\[foo]" "#[foo]") nil)
+               ))
+  (should (not (equal (string-match "#\\[(foo)]" "#[(foo)]") nil)
+               ))
+  (should (not (equal (string-match "#\\[derive(Component)]" "#[derive(Component)]") nil)
+               ))
+  (should (not (equal (string-match "#\\[derive(.*Component.*)]" "#[derive(Component, Debug, Serialize, Deserialize, Clone)]") nil)))
+  (should (not (equal (string-match "#\\[derive(.*Component.*)]" "#[derive(Debug, Serialize, Component, Deserialize, Clone)]") nil)))
+  )
+
+(ert-deftest extract-between-curly-brackets ()
+  (with-temp-buffer
+    (insert-file-contents "componentes-for-elisp.rs")
+    (goto-char 1)
+    (should (equal (mh/components) ("Player" "Hitpoints" "NPCs")))
+    )
+  )
+
+(defun mh/ert()
+  "Runs eval-buffer and ert"
+  (interactive)
+  (save-excursion
+    (eval-buffer)
+    (ert t)
+    )
+  )
+
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1))))
+  )
+
